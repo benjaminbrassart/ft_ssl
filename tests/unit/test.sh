65e9c9c790efc0e15afc7fe3fa2e3d27116b4a97
Benjamin Brassart <benjaminbrassart@outlook.com>
2023-05-25 22:53:42 +0200

    Add exit status for unit test script

--- tests/unit/test.sh
+++ tests/unit/test.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env sh
 
+make -sj || exit 2
+
+STATUS=0
+
 for f in $(find . -name '*.out'); do
     basename -z "${f}" .out
     printf -- ' -> '
     if "$f"; then
         printf -- 'OK!\n'
+    else
+        STATUS=1
     fi
 done
+
+exit "${STATUS}"
