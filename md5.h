9c6f271e6bd65d50e235c84f3cae99feeae9ebf4
Benjamin Brassart <bbrassar@student.42.fr>
2025-05-30 09:33:01 +0200

    Add md5 prototypes

---
+++ md5.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   md5.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/30 09:30:36 by bbrassar          #+#    #+#             */
+/*   Updated: 2025/05/30 09:32:33 by bbrassar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define MD5_BLOCK_SIZE 32
+#define MD5_DIGEST_SIZE 16
+
+struct md5_context {
+	uint32_t a, b, c, d;
+	uint8_t buffer[MD5_BLOCK_SIZE];
+	uint64_t length;
+};
+
+void md5_init(struct md5_context *ctx);
+
+void md5_update(struct md5_context *ctx, void const *data, size_t len);
+
+void md5_digest(struct md5_context *ctx, uint8_t digest[MD5_DIGEST_SIZE]);
