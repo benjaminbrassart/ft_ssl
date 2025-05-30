e743d080ba750f1072f17a59805d709344884169
Benjamin Brassart <bbrassar@student.42.fr>
2025-05-30 11:14:56 +0200

    Add missing fields for sha256_context

--- sha256.h
+++ sha256.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 11:05:58 by bbrassar          #+#    #+#             */
-/*   Updated: 2025/05/30 11:07:26 by bbrassar         ###   ########.fr       */
+/*   Updated: 2025/05/30 11:14:30 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define SHA256_DIGEST_SIZE 32
+#define SHA256_BLOCK_SIZE 64
 #define SHA256_DIGEST_SIZE 32
 
-struct sha256_context {};
+struct sha256_context {
+	uint32_t a, b, c, d, e, f, g, h;
+	uint8_t buffer[SHA256_BLOCK_SIZE];
+	uint64_t length;
+};
 
 void sha256_init(struct sha256_context *ctx);
 
