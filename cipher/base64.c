dabf08dba045c9569061dd6f159d362aebadfce4
Benjamin Brassart <bbrassar@student.42.fr>
2025-06-02 14:56:36 +0200

    Add full block base64 decoder

--- cipher/base64.c
+++ cipher/base64.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 12:46:54 by bbrassar          #+#    #+#             */
-/*   Updated: 2025/06/02 13:38:50 by bbrassar         ###   ########.fr       */
+/*   Updated: 2025/06/02 14:57:45 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,43 @@ static void encode_full_block(uint8_t const in[3], char out[4])
 	out[1] = BASE64[((in[0] & 0x03) << 4) | (in[1] >> 4)];
 	out[2] = BASE64[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
 	out[3] = BASE64[in[2] & 0x3f];
+}
+
+static int char_to_sextet(char c, uint8_t *sextet)
+{
+	if (c >= 'A' && c <= 'Z') {
+		*sextet = c - 'A';
+	} else if (c >= 'a' && c <= 'z') {
+		*sextet = (c - 'a') + 26;
+	} else if (c >= '0' && c <= '9') {
+		*sextet = (c - '0') + 52;
+	} else if (c == '+') {
+		*sextet = 62;
+	} else if (c == '/') {
+		*sextet = 63;
+	} else {
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+static int decode_full_block(char const in[4], uint8_t out[3])
+{
+	uint8_t sextets[4];
+
+	for (int i = 0; i < 4; i += 1) {
+		int result;
+
+		result = char_to_sextet(in[i], &sextets[i]);
+		if (result != EXIT_SUCCESS) {
+			return result;
+		}
+	}
+
+	out[0] = (sextets[0] << 2) | (sextets[1] >> 4);
+	out[1] = (sextets[1] << 4) | (sextets[2] >> 2);
+	out[2] = (sextets[2] << 6) | sextets[3];
+
+	return EXIT_SUCCESS;
 }
--- cipher/base64_test.c
+++ cipher/base64_test.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 13:08:07 by bbrassar          #+#    #+#             */
-/*   Updated: 2025/06/02 13:32:53 by bbrassar         ###   ########.fr       */
+/*   Updated: 2025/06/02 14:56:03 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,12 @@ struct base64_test {
 
 static struct base64_test const BLOCK_TESTS[] = {
 	{
-		.decoded = { '\0', '\0', '\0' },
+		.decoded = { 0x00, 0x00, 0x00 },
 		.encoded = { 'A', 'A', 'A', 'A' },
+	},
+	{
+		.decoded = { 0xff, 0xff, 0xff },
+		.encoded = { '/', '/', '/', '/' },
 	},
 	{
 		.decoded = { 'M', 'a', 'n' },
@@ -54,9 +58,31 @@ static int test_encode_block(struct base64_test const *test)
 	return EXIT_SUCCESS;
 }
 
+static int test_decode_block(struct base64_test const *test)
+{
+	uint8_t out[3];
+	int res = decode_full_block(test->encoded, out);
+
+	if (res != EXIT_SUCCESS || memcmp(out, test->decoded, 3) != 0) {
+		fprintf(stderr, "Decode block test failed:\n");
+		fprintf(stderr, "  Input: \"%.4s\"\n", test->encoded);
+		fprintf(stderr, "\n");
+		fprintf(stderr, "  Want: %02x %02x %02x\n", test->decoded[0],
+			test->decoded[1], test->decoded[2]);
+		fprintf(stderr, "  Have: %02x %02x %02x\n", out[0], out[1],
+			out[2]);
+		fprintf(stderr, "  Return code: %d\n", res);
+		fprintf(stderr, "\n");
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
+
 int main(void)
 {
 	for (size_t i = 0; i < sizeof_array(BLOCK_TESTS); i += 1) {
 		test_encode_block(&BLOCK_TESTS[i]);
+		test_decode_block(&BLOCK_TESTS[i]);
 	}
 }
